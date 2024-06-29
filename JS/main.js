8614d7338e79d8f4e6c7290b1cf77a526630ef70
katakori619 <kmdht9012@gmail.com>
2024-06-29 03:15:57 +0300

    amogos

---
+++ JS/main.js
@@ -0,0 +1,99 @@
+async function getGames(categoryOption) {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "b7ddeaba2emsh4c7f9a279de19b4p17f62fjsn9ef33d17e7d9",
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+  
+    const api = await fetch(
+      `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${categoryOption}`,
+      options
+    );
+    const response = await api.json();
+    displayGames(response)
+    // console.log(response);
+}  
+
+function displayGames(response){
+    let cartona = ``
+    for(var i = 0;i<response.length;i++){
+        cartona += `
+        <div id="${response[i].id}" class="card mx-2" style="width: 19rem;">
+                <div class="image card-img-top mt-3 position-relative">
+                  <img src="${response[i].thumbnail}" class="w-100" alt="...">
+                </div>
+                <div class="card-body">
+                  <div class="card-title d-flex justify-content-between align-items-center">
+                    <span class="name small small">${response[i].title}</span>
+                    <span class="badge text-bg-primary p-2">free</span>
+                  </div>
+                  <p class="card-text text-center">${response[i].short_description}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content-between">
+                    <span>${response[i].genre}</span>
+                    <span>${response[i].platform}</span>
+                  </li>
+                </ul>
+              </div>
+        `
+    }
+    document.getElementById('content').innerHTML = cartona
+}
+async function getDetails(gameid){
+    const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "b7ddeaba2emsh4c7f9a279de19b4p17f62fjsn9ef33d17e7d9",
+          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+        },
+      };
+    
+      const api = await fetch(
+        `https://free-to-play-games-database.p.rapidapi.com/api/games?id=${gameid}`,
+        options
+      );
+      const response = await api.json();
+      displayDetails(response)
+      // console.log(response);
+}
+function displayDetails(response){
+    let cartona = ``
+    cartona += `
+    <div class="col-md-4">
+            <img src="${response.thumbnail}" class="w-100" alt="image details">
+            </div>
+            <div class="col-md-8">
+              <h3>Title: ${response.title}</h3>
+              <p>Category: <span class="badge text-bg-info"> ${response.genre}</span> </p>
+              <p>Platform: <span class="badge text-bg-info"> ${response.platform}</span> </p>
+              <p>Status: <span class="badge text-bg-info"> ${response.status}</span> </p>
+              <p class="small">${response.description}</p>
+            <a class="btn btn-outline-warning text-white" target="_blank" href="${response.game_url}">Show Game</a>
+            </div>
+    `
+    document.getElementById('detailsContent') = cartona
+}
+document.querySelectorAll(".menu a").forEach((link) => {
+    getGames('mmorpg');
+    link.addEventListener("click", (e) => {
+       document.querySelector(".menu .active").classList.remove("active");
+       e.target.classList.add("active");
+       this.getGames(e.target.innerHTML);
+    });
+ });
+
+document.getElementById("btnClose").addEventListener("click", () => {
+    document.querySelector(".games").classList.remove("d-none");
+    document.querySelector(".details").classList.add("d-none");
+});
+document.querySelectorAll(".card").forEach((item) => {
+    console.log(item)
+    item.addEventListener("click", () => {
+        const id = item.id;
+        console.log(id);
+    //   this.getDetails(id);
+       });
+    });
